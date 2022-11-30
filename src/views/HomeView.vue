16b9d44259d6399e58fed1a17b8231ca7bdb94e5
wisam ali <wisamah84@gmail.com>
2022-12-01 01:43:40 +0300

    simple Get with Vue3 and Axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <div class="container text-center" >
+  <div class="row" >
+    <div class="col-md-4 col-sm-12 mb-4 position-relative" v-for="post in posts" :key="post.id">
+      <div class="card h-100 mb-4 border border-info" style="width: 18rem;">
+   <img :src="post.image" class="card-img-top p-3" alt="..." width="100" height="250">
+  <div class="card-body">
+    <h5 class="card-title">{{post.title}}</h5>
+    <h5 class="card-title text-danger">$ {{post.price}}</h5>
+    <p class="card-text h-50">{{post.description}}</p>
+    <a href="#" class="btn btn-success position-absolute bottom-0 end-75 mb-3" @click="singlePost(post.id)" >more details</a>
+    
+  </div>
+</div>
+    </div>
+    
+  </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+posts:[],
+
+    }
+  },
+  created(){
+    axios.get("https://fakestoreapi.com/products").then((res)=>{
+     this.posts= res.data
+    })
+
+  },
+  methods:{
+    singlePost(idd){
+      axios.get("https://fakestoreapi.com/products/"+idd).then((res)=>{
+        console.log( res.data)
+    })
+   
   }
 }
+}
+
 </script>
+<style>
+  
+</style>
